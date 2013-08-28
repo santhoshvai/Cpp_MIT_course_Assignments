ca03a6586e133c2a3da5f5896020cce077c12e6b
santhoshvai <santhoshvai@gmail.com>
2013-08-29 00:05:42 +0200

    deleted some files

--- assignment3_6.S096/2/main.c
+++ assignment3_6.S096/2/main.c
@@ -1,16 +1,6 @@
-#include "amaze.h"
-// find seven different ways to print the odd numbers between 0 and 10 on a single line. You should give them names like amaze1, amaze2
-// using variety of control structures for looping, such as for, while, do/while, goto, and several for testing conditions, such as if and switch.
  
  int main(int argc, char *argv[]) {	
 	
-	amaze1();
-    amaze2();
-	amaze3();
-	amaze4();
-	amaze5();
-	amaze6();
-	amazeWOW();
 	return 0;
 }
 
