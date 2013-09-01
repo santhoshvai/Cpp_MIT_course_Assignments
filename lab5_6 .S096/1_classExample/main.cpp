437a78b3be82b55346bbe3d9714517fd5bf735bb
santhoshvai <santhoshvai@gmail.com>
2013-09-01 17:56:58 +0200

    assignment 6 will be left unsolved.. its boring and easy

--- lab5_6 .S096/1_classExample/main.cpp
+++ lab5_6 .S096/1_classExample/main.cpp
@@ -7,25 +7,19 @@ int * height;
 public:
 Rectangle(int, int); // constructor
 ~Rectangle(); // destructor
+
 void printMe(){ // 'method' / member function
-
 printf("Dimensions: %d by %d.\n", *width, *height);
-
 }
 
 };
 Rectangle::Rectangle(int w, int h){
 
 // constructor definition
-
 width = new int;
-
 height = new int;
-
 *width = w;
-
 *height = h;
-
 }
 
 Rectangle::~Rectangle(){
@@ -34,9 +28,7 @@ delete height;
 }
 
 int main(){
-
 Rectangle box(5, 7);
-
 box.printMe();
 
 }
