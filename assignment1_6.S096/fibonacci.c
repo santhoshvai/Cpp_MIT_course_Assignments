930306a32ba982018c0a30ddabafe8796fa66806
Santhosh Vaiyapuri <santhoshvai@gmail.com>
2014-09-11 11:35:48 +0200

    O(1) space Fibonacci

--- assignment1_6.S096/fibonacci.c
+++ assignment1_6.S096/fibonacci.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include "fibonacci.h"
+
 void print_fib(int n) {
 	printf("%d\n", fib(n));
 }
 
 int fib(int n) {
-	int dp[n+1];
-	dp[0]=0;dp[1]=1;
-	for(i=2;i<=n;i++){
-		dp[i]=dp[i-1]+dp[i-2];
+	if (n < 2) return n;
+	int firstFib = 0;
+	int secondFib = 1;
+	int currentFib = 0;
+	int i;
+	for(i=2; i<=n; i++) {
+		currentFib = firstFib + secondFib;
+		firstFib = secondFib;
+		secondFib = currentFib;
 	}
-	return dp[n];
+	return currentFib;
 }
